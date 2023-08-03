26da8bc0f9b24b3d715ad814048461f5183a3896
Stanislav <stanislav.yas@gmail.com>
2023-08-03 17:19:00 +0600

    read proxy_list from file

--- util/proxy/list_proxy_provider.py
+++ util/proxy/list_proxy_provider.py
@@ -3,28 +3,30 @@ import random
 import requests
 class ListProxyProvider(ProxyProvider):
 
-    def __init__(self, proxy_list_str: str = '') -> None:
+    def __init__(self, proxy_list_raw_str: str = '') -> None:
+        '''ListProxyProvider created from multiline proxy list string (ex. read from file)'''
         super().__init__()
         self.type = Proxy.LIST_PROXY_TYPE
         self.proxies = []
-        '''Array of available proxies. Proxy is an array of [ip, port, login. password]'''        
+        '''Array of available proxies. Proxy is an array of [protocol, ip, port, login. password]'''        
         self.blocked_proxies = []
         '''Array of blocked (unavailable) proxies'''
         self.proxy_index = -1
         '''Index of current proxy. -1 if not set yet '''        
-        proxy_list = proxy_list_str.splitlines()
+        proxy_list = proxy_list_raw_str.splitlines()
         for proxy in proxy_list:
             try:
                 cnt = proxy.count(':')
-                if cnt == 1 or cnt == 3:
+                if cnt == 2 or cnt == 4:
                     self.proxies.append(proxy.strip().split(':'))            
             except:
-                 continue          
+                continue          
         if len(self.proxies) == 0:
-            raise Exception('Empty proxy pool')    
+            raise Exception('Proxies absent')    
 
     @property
     def proxies_count(self):
+        '''Count of the available proxies'''
         return len(self.proxies)
     
     @property
@@ -66,39 +68,45 @@ class ListProxyProvider(ProxyProvider):
 
         
     def check_proxy(self, timeout=10) -> bool:
+        '''Checking availability of current proxy at 'https://httpbin.org/ip' '''
         proxy = self.proxy
         if proxy == None: return False
-        if len(proxy) >= 4:
-            proxy_url = f'{proxy[2]}:{proxy[3]}@{proxy[0]}:{proxy[1]}'
-        elif len(proxy) >= 2:
-            proxy_url = f'{proxy[0]}:{proxy[1]}'
+        if len(proxy) >= 5:
+            proxy_url = f'{proxy[3]}:{proxy[4]}@{proxy[1]}:{proxy[2]}'
+        elif len(proxy) >= 3:
+            proxy_url = f'{proxy[1]}:{proxy[2]}'
         else: return False
         proxies = {
             # 'http': f'http://{proxy_url}',
             # 'https': f'https://{proxy_url}',
             # 'http': f'socks5://{proxy_url}',
             # 'https': f'socks5://{proxy_url}',
-            'all': f'socks5://{proxy_url}',          
+            'all': f'{proxy[0]}://{proxy_url}',          
         }
         try:
-            print(f'Checking proxy: {proxy}...')
+            print(f'Checking proxy: {self.proxy_str}...')
             response = requests.get('https://httpbin.org/ip', proxies=proxies, verify=True, timeout=timeout)
             print(f'Checking elapsed {response.elapsed.seconds} seconds, status_code = {response.status_code}')
             if response.status_code == requests.codes.ok:
                 origin = response.json()['origin']
-                if origin == proxy[0]:
-                    print(f'Checking success, origin {origin}')
+                if origin == proxy[1]:
+                    print(f'Checking success, origin: {origin}')
                     return True
                 else:
-                    print(f'Checking failed, origin {origin}')
+                    print(f'Checking failed, origin: {origin}')
                     return False
         except Exception as err:
             print(err)
         print('Checking failed')
         return False
     
+    @property
+    def proxy_str(self):
+        '''String representation of proxy w/o login,pwd'''
+        return f'{self.proxy[0]}//{self.proxy[1]}:{self.proxy[2]}' if self.proxy != None else None
+    
     def __repr__(self) -> str:
-        return f"ListProxyProvider (proxies: {len(self.proxies)+len(self.blocked_proxies)}, blocked: {len(self.blocked_proxies)}, current: {self.proxy})"
+        return f"ListProxyProvider (proxies: {len(self.proxies)+len(self.blocked_proxies)}, blocked: {len(self.blocked_proxies)}, current: {self.proxy_str})"
 
 sample_proxy_list_str = '''
 #gjgjfgsjfh
@@ -108,34 +116,24 @@ sample_proxy_list_str = '''
 77.83.118.44:59101:staniaskz:WiRhdJv7ty
 94.137.90.126:59101:staniaskz:WiRhdJv7ty
 '''
-sample_proxy_list_str2 = '''
+socks5_proxy_list_str = '''
 #shdgfsjhgjfgdjh
-94.137.90.126:59100
-109.172.113.130:59100
-93.188.207.49:59100	
-212.8.229.77:59100
-77.83.118.44:59100
+socks5:94.137.90.126:59101:staniaskz:WiRhdJv7ty	
+socks5:109.172.113.130:59101:staniaskz:WiRhdJv7ty	
+socks5:93.188.207.49:59101:staniaskz:WiRhdJv7ty	
+socks5:212.8.229.77:59101:staniaskz:WiRhdJv7ty	
+socks5:77.83.118.44:59101:staniaskz:WiRhdJv7ty	
+
 '''
 
 if __name__ == '__main__' :
-    pp1 = ListProxyProvider(proxy_list_str=sample_proxy_list_str)
+    socks5_proxy_list_str = open('util/proxy/proxy_list.txt').read()
+    pp1 = ListProxyProvider(proxy_list_raw_str=socks5_proxy_list_str)
     print(pp1)
-    # pp2 = ListProxyProvider(proxy_list_str=sample_proxy_list_str2)
-    for proxy in pp1.all_proxies:
+    for i in range(0, pp1.proxies_count):
         if not pp1.check_proxy():
             pp1.block_proxy()
         else:
             pp1.change_proxy()
     print(pp1)
-    # print(pp1.next_proxy())
-    # print(pp1.next_proxy(is_random=True))
-    # pp1.check_proxy()
-    # print(pp1.next_proxy(is_random=True))
-    # pp1.check_proxy()
-    # print(pp1.next_proxy())
-    # print(pp1.next_proxy())
-    # print(pp1.next_proxy())
-    # pp2 = ListProxyProvider(proxy_list_str=sample_proxy_list_str)
-    # print(pp1.current_proxy())
-    # print(pp2.current_proxy())
     pass
