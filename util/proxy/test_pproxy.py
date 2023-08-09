cc2264f267b0fc9d44f098f59fb9034a23a67d70
Stanislav <stanislav.yas@gmail.com>
2023-08-09 19:27:34 +0600

    save@09.08.23

--- util/proxy/test_pproxy.py
+++ util/proxy/test_pproxy.py
@@ -1,21 +1,48 @@
 import asyncio
 import pproxy
+import time
 
-# pproxy -l http://:8888 -r socks5://93.188.207.49:59101#staniaskz:WiRhdJv7ty
-server = pproxy.Server('http://:8888')
-remote = pproxy.Connection('socks5://93.188.207.49:59101#staniaskz:WiRhdJv7ty')
-args = dict( rserver = [remote],
-             verbose = print
-            )
+async def run_proxy():
+    # pproxy -l http://:8888 -r socks5://93.188.207.49:59101#staniaskz:WiRhdJv7ty
+    server = pproxy.Server('socks5://:8888')
+    remote = pproxy.Connection('socks5://93.188.207.49:59101#staniaskz:WiRhdJv7ty')
+    args = dict( rserver = [remote],
+                verbose = print
+                )
+    print('Serving on', server.bind, 'by', ",".join(i.name for i in server.protos) + ('(SSL)' if server.sslclient else ''), '({}{})'.format(server.cipher.name, ' '+','.join(i.name() for i in server.cipher.plugins) if server.cipher and server.cipher.plugins else '') if server.cipher else '')
+    loop = asyncio.get_event_loop()
+    handler = await server.start_server(args)
+    try:
+        print("Loop started")
+        loop.run_forever()
+        print("Loop stopping...")
+    except KeyboardInterrupt:
+        print('exit!')
+    finally:
+        print("Loop closing...")
+        handler.close()
+        await handler.wait_closed()
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+        print("Loop closed")
 
-loop = asyncio.get_event_loop()
-handler = loop.run_until_complete(server.start_server(args))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    print('exit!')
+async def mytask():
+    print("mytask will be running 15 sec.")
+    result = await asyncio.sleep(15, result=555)
+    return result
 
-handler.close()
-loop.run_until_complete(handler.wait_closed())
-loop.run_until_complete(loop.shutdown_asyncgens())
-loop.close()
+async def main():
+    # Create a "cancel_me" Task
+    task = asyncio.create_task(mytask(), name='mytask')
+
+    # Wait for 5 second
+    await asyncio.sleep(5)
+
+    # task.cancel()
+    try:
+        await task
+        print(f"mytask finished successfully with result={task.result()}")
+    except asyncio.CancelledError:
+        print(f"main(): mytask is cancelled now with result=")
+
+asyncio.run(main())
